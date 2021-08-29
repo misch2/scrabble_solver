5e833fb7335e0a71864f67a856315b59b07f3d44
Michal Schwarz <michal.schwarz@gmail.com>
2021-08-29 19:36:58 +0200

    javafx first working version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 package main.java;
 
 import java.util.*;
-//import java.io.*;
 
-// import org.apache.commons.codec.binary.Hex;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 //import javafx.scene.input.*;
@@ -30,23 +28,30 @@ public class Main extends Application {
     @FXML
     private ListView<String> dstList;
 
+    @FXML 
+    void liveSolve() {
+        solve();
+    }
+
     @FXML
     void solve() {
         String letters = srcLetters.getText();
-        System.out.println(letters);
-
+        
         letters = scrabble.normalizeCase(letters);
-        srcLetters.setText(letters);
+        // srcLetters.setText(letters); // didn't behave nicely, this would always reset cursor position
 
         List<WordScoreResult> list = scrabble.bestAvailableWordList(letters);
         dstList.getItems().clear();
         if (list.isEmpty()) {
             dstList.getItems().add(" - no solution for this input");
         } else {
+            String remaining = "";
             for (WordScoreResult res : list) {
                 dstList.getItems().add(String.format(" - %s (%d pt)", res.getWord(), res.getScore()));
+                remaining = res.getRemainingChars();
             }
-        }
+            dstList.getItems().add("Remaining charactes: " + remaining);
+       }
     }
 
     public static void main(String[] args) {
@@ -56,52 +61,6 @@ public class Main extends Application {
         Application.launch(args);
         System.exit(0);
 
-        // // FIXME remove the rest:
-        // Locale locale = Locale.forLanguageTag("cs-CZ");
-        // System.out.println("locale: " + locale);
-
-        // try {
-        // OutputStreamWriter oswDef = new OutputStreamWriter(System.out);
-        // System.out.println("Implicitni kodovani konzole: " + oswDef.getEncoding());
-
-        // /* IBM852 je výstupní kódování češtiny v DOSovém okénku */
-        // OutputStreamWriter osw = new OutputStreamWriter(System.out, "IBM852");
-        // System.out.println("Nastavene kodovani konzole: " + osw.getEncoding());
-
-        // String l = new Scanner(System.in, "IBM852").nextLine();
-        // System.out.println("1: len=" + l.length() + ", str=" +
-        // Hex.encodeHexString(l.getBytes()));
-
-        // l = System.console().readLine();
-        // System.out.println("2: len=" + l.length() + ", str=" +
-        // Hex.encodeHexString(l.getBytes()));
-
-        // Scrabble scrabble = new Scrabble();
-        // scrabble.loadAllFromFolder("cz_nodia");
-
-        // System.out.println("Which letters do you have?");
-        // String letters = new BufferedReader(new InputStreamReader(System.in,
-        // "UTF-8")).readLine();
-        // System.out.println(letters);
-
-        // letters = scrabble.normalizeCase(letters);
-        // System.out.println(String.format("Best set of words for a set of [%s]:",
-        // letters));
-        // List<WordScoreResult> list = scrabble.bestAvailableWordList(letters);
-        // if (list.isEmpty()) {
-        // System.out.println(" - no solution for this input");
-        // } else {
-        // for (WordScoreResult res : list) {
-        // System.out.println(String.format(" - %s (%d pt)", res.getWord(),
-        // res.getScore()));
-        // }
-        // }
-        // // String.format("Best set of words: %s",
-        // // scrabble.bestAvailableWordList(letters)));
-        // } catch (
-
-        // Exception e) {
-        // e.printStackTrace();
-        // }
+        Locale.setDefault(Locale.forLanguageTag("cs-CZ"));
     }
 }
